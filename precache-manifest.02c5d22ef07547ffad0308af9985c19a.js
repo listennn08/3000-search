a8a525b89049de695e840ba73c6dd0f74d8b1ae7
MattTai <listennn08@outlook.com>
2020-07-20 13:53:12 +0800

    Updates

---
+++ precache-manifest.02c5d22ef07547ffad0308af9985c19a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff8e9f1c90fe8ae7a071fa84d6e45b7",
+    "url": "/3000-search/index.html"
+  },
+  {
+    "revision": "58a2794a5ed779cf9712",
+    "url": "/3000-search/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ca48025ae0b287f251d6",
+    "url": "/3000-search/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "58a2794a5ed779cf9712",
+    "url": "/3000-search/static/js/2.289c9335.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/3000-search/static/js/2.289c9335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca48025ae0b287f251d6",
+    "url": "/3000-search/static/js/main.23a7fa0b.chunk.js"
+  },
+  {
+    "revision": "a71c265d39cf223fff08",
+    "url": "/3000-search/static/js/runtime-main.343ad78b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/3000-search/static/media/logo.ee7cd8ed.svg"
+  }
+]);
